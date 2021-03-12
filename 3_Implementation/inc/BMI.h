f00a01272c4225c638f933276f602003589af56a
99003775 <78869692+99003775@users.noreply.github.com>
2021-03-12 22:15:36 +0530

    modify code

--- 3_Implementation/inc/BMI.h
+++ 3_Implementation/inc/BMI.h
@@ -1,9 +1,10 @@
 #ifndef __BMI_H__
 #define __BMI_H__
 #include<stdio.h>
-void Calc_BMI();
-float BMI(float,float);
-/*float BMI2(int,float);
-float BMI3(int,float);
-float BMI4(int,float);*/
+float Kg_Meter(int,float);
+float Kg_Cm(int,float);
+float Lbs_Cm(int,float);
+float Lbs_Meter(int,float);
+
+
 #endif
