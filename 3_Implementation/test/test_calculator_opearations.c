ad033690588472ba1b006d01ddc228bdcdfac0d7
99003765 <78848640+99003765@users.noreply.github.com>
2021-03-13 11:02:18 +0530

    test cases added

--- 3_Implementation/test/test_calculator_opearations.c
+++ 3_Implementation/test/test_calculator_opearations.c
@@ -1,5 +1,5 @@
 #include "unity.h"
-#include "calculator_operations.h"
+#include "operation.h"
 #include"trignometry.h"
 
 /* Modify these two lines according to the project */
@@ -8,11 +8,12 @@
 
 /* Prototypes for all the test functions */
 void test_add(void);
-void test_add_testcase2(void);
+
 void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
 void test_trig(void);
+void test_trig1(void);
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -27,11 +28,12 @@ int main()
 
 /* Run Test functions */
   RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
+
   RUN_TEST(test_subtract);
   RUN_TEST(test_multiply);
   RUN_TEST(test_divide);
   RUN_TEST(test_trig);
+   RUN_TEST(test_trig1);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
@@ -42,33 +44,31 @@ void test_add(void) {
   TEST_ASSERT_EQUAL(30, add(10, 20));
   TEST_ASSERT_EQUAL(-10, add(10, -20));
 }
-void test_add_testcase2(void) {
- 
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1500, add(750, 7500));
-}
+
 void test_subtract(void) {
   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
   
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, subtract(1000, 900));
+
 }
 
 void test_multiply(void) {
   TEST_ASSERT_EQUAL(0, multiply(1, 0));
   
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(2, multiply(2, 5));
+ 
 }
 
 void test_divide(void) {
   TEST_ASSERT_EQUAL(0, divide(1, 0));
   
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(3, divide(2, 2));
+
 }
 void test_trig(void) {
-  TEST_ASSERT_EQUAL(1.00, sin(90));
-  TEST_ASSERT_EQUAL(1.154701, sec(30));
+  TEST_ASSERT_EQUAL(0.893997, sin_angle(90));
+  
+  
+}
+void test_trig1(void) {
+  TEST_ASSERT_EQUAL(0, cos_angle(90));
+  
   
 }
