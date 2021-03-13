db50f48ffc664d4f429425512ad98e3216f06de6
99003775 <78869692+99003775@users.noreply.github.com>
2021-03-13 14:02:08 +0530

    Update BMI.c

--- 3_Implementation/src/BMI.c
+++ 3_Implementation/src/BMI.c
@@ -1,246 +1,27 @@
 #include "BMI.h"
-
-int main()
+bmi_t bmicon(int h,int w)
 {
- int weight;
- float height;
- char operator;
- 
- 
- 
- /// printf(" 1 for enter your height in cm");
- // printf(" 2 for enter your height in m");
- 
- 
- printf("Press 1 to enter heihgt in Meter and weight in kg \n press 2 for enter your height in CM and weight in kg \n press 3 for enter your weight in LBS and height in cm \n press 4 to enter your weight in lbs and height in meter ");
- scanf("%c", &operator);
- 
- 
- 
- 
-switch(operator)
-{
- case '1':
- 
- printf("please enter your weight in kg:");
- printf("\n");
- scanf("%d",&weight);
- printf("please enter your height in Meter:");
- printf("\n");
- scanf("%f",&height);
- 
- Kg_Meter(weight,height);
- break;
- 
- 
- case '2':
- 
- printf("please enter your weight in kg:");
- printf("\n");
- scanf("%d",&weight);
- printf("please enter your height in cm:");
- printf("\n");
- scanf("%f",&height);
- 
- 
- int wig=weight*1000;
- 
- 
- Kg_Cm(wig,height);
- break;
- 
- case '3':
- 
- 
- printf("please enter your weight in lbs:");
- printf("\n");
- scanf("%d",&weight);
- printf("please enter your height in cm:");
- printf("\n");
- scanf("%f",&height);
- Lbs_Cm(weight,height);
- break;
- 
- case '4':
- 
- 
- printf("please enter your weight in lbs:");
- printf("\n");
- scanf("%d",&weight);
- printf("please enter your height in meter:");
- printf("\n");
- scanf("%f",&height);
- Lbs_Meter(weight,height);
- break;
- 
- default:
- printf("enter valid choic");
- 
-}
+    int answer;
+    int squ=w*w;
+    answer=h/squ;
+    if(answer<18)
+    {
+        return underwt;
 
-}
+    }
+    else if (answer<24)
+    {
+        /* code */
+        return normalwt;
+    }
+    else if(answer<29)
+    {
+        return overwt;
 
-float Kg_Meter(flaot weight, float height) {
- float result;
- if(weight<=10)
- {
- printf("Please enter weight greater then 10kg ");
- }
- if( height<=0.8)
- {
- printf("please enter height greater then 9 Meter");
- }
- printf("\n");
- float x=height*height;
- // printf("%.3f",x);
- // printf("\n");
- // printf("%d",weight);
- // printf("\n");
- result= weight/(float)x;
- 
- 
- printf("%.3f",result);
- printf("\n");
- 
- if(result<18.5)
- {
- printf("Underweight");
- }
- else if(result>18.5&&result<24.9)
- {
- printf("Normal and healthy Weight");
- }
- else if(result>25.0 && result<29.9)
- {
- printf("Over weight");
- }
- else
- {
- printf("Obses");
- }
-}
+    }
+    else
+    {
+        return obesity;
 
-
-float Kg_Cm(float wig, float height) {
- float result;
- if(wig<=10000)
- {
- printf("Please enter weight greater then 10kg ");
- }
- if( height<=80)
- {
- printf("please enter height greater then 85");
- }
- printf("\n");
- float x=height*height;
- // printf("%.3f",x);
- // printf("\n");
- // printf("%d",wig);
- // printf("\n");
- result= wig/(float)x*10;
- 
- 
- printf("%.1f",result);
- printf("\n");
- 
- if(result<18.5)
- {
- printf("Underweight");
- }
- else if(result>18.5&&result<24.9)
- {
- printf("Normal and healthy Weight");
- }
- else if(result>25.0 && result<29.9)
- {
- printf("Over weight");
- }
- else
- {
- printf("Obses");
- 
- }
+    }
 }
- 
- 
- float Lbs_Cm(float weight, float height) {
- float result;
- if(weight<=24)
- {
- printf("Please enter weight greater then 24lbs ");
- }
- if( height<=80)
- {
- printf("please enter height greater then 85");
- }
- printf("\n");
- float x=height*height;
- // printf("%.3f",x);
- // printf("\n");
- // printf("%d",wig);
- // printf("\n");
- result=730* (weight/(float)x*10);
- 
- 
- printf("%.1f",result);
- printf("\n");
- 
- if(result<18.5)
- {
- printf("Underweight");
- }
- else if(result>18.5&&result<24.9)
- {
- printf("Normal and healthy Weight");
- }
- else if(result>25.0 && result<29.9)
- {
- printf("Over weight");
- }
- else
- {
- printf("Obses");
- }
- }
- 
- 
- float  Lbs_Meter(float weight, float height) {
- float result;
- if(weight<=24)
- {
- printf("Please enter weight greater then 24lbs ");
- }
- if( height<=0.8)
- {
- printf("please enter height greater then 9m");
- }
- printf("\n");
- int height1=height*100;
- float x=height1*height1;
- // printf("%.3f",x);
- // printf("\n");
- // printf("%d",wig);
- // printf("\n");
- result=730* (weight/(float)x*10);
- 
- 
- printf("%.1f",result);
- printf("\n");
- 
- if(result<18.5)
- {
- printf("Underweight");
- }
- else if(result>18.5&&result<24.9)
- {
- printf("Normal and healthy Weight");
- }
- else if(result>25.0 && result<29.9)
- {
- printf("Over weight");
- }
- else
- {
- printf("Obses");
- }
- }
